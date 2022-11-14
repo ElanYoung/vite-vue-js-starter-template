f1dfa23ff82f3b77556135b6ddacf665aaf0f9c7
ElanYoung <991658923@qq.com>
2022-11-14 21:48:37 +0800

    refactor: optimized code

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,14 +2,14 @@
  * 是否为开发环境
  * @returns {Boolean}
  */
-export function isDev() {
+export const isDev = () => {
   return import.meta.env.DEV;
-}
+};
 
 /**
  * 是否为生产环境
  * @returns {Boolean}
  */
-export function isProd() {
+export const isProd = () => {
   return import.meta.env.PROD;
-}
+};
