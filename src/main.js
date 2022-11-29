e5492be3a1f286cabf96dd881e0b0a7f45cfe15e
ElanYoung <991658923@qq.com>
2022-11-29 21:00:00 +0800

    fix: component library style loss after build
    目前采用全局引入样式资源

--- src/main.js
+++ src/main.js
@@ -7,4 +7,7 @@ import store from './store';
 import '@/assets/main.css';
 import '@/style/index.less';
 
+// 引入组件库全局样式资源
+import 'tdesign-vue-next/es/style/index.css';
+
 createApp(App).use(router).use(store).mount('#app');
