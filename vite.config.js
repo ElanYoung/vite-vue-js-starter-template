23c91b70f465f2fec1a0cd134bb7bc580ac175f9
ElanYoung <991658923@qq.com>
2023-11-25 17:11:44 +0800

    feat: vite 4 -> vite 5

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { TDesignResolver } from 'unplugin-vue-components/resolvers';
@@ -13,18 +15,32 @@ export default ({ mode }) => {
     plugins: [
       vue(),
       AutoImport({
+        imports: ['vue', 'vue-router', 'pinia'],
         resolvers: [
           TDesignResolver({
             library: 'vue-next',
           }),
+          IconsResolver({
+            prefix: 'Icon',
+          }),
         ],
+        eslintrc: {
+          enabled: true,
+        },
+        dts: true,
       }),
       Components({
         resolvers: [
           TDesignResolver({
             library: 'vue-next',
           }),
+          IconsResolver({
+            enabledCollections: ['lets-icons'],
+          }),
         ],
+      }),
+      Icons({
+        autoInstall: true,
       }),
     ],
     resolve: {
@@ -43,9 +59,11 @@ export default ({ mode }) => {
         },
       },
     },
+    optimizeDeps: {
+      include: ['mitt', 'dayjs', 'axios', 'pinia', '@vueuse/core'],
+      exclude: ['@iconify-icons/lets-icons'],
+    },
     server: {
-      // 是否开启 https
-      https: false,
       // 端口号
       port: VITE_PORT,
       // 监听所有地址
@@ -56,6 +74,10 @@ export default ({ mode }) => {
       cors: true,
       // 自定义代理规则
       proxy: {},
+      // 预热文件以提前转换和缓存结果，降低启动期间的初始页面加载时长并防止转换瀑布
+      warmup: {
+        clientFiles: ['./index.html', './src/{views,components}/*'],
+      },
     },
     build: {
       // 设置最终构建的浏览器兼容目标
@@ -66,6 +88,23 @@ export default ({ mode }) => {
       chunkSizeWarningLimit: 2000,
       // 启用/禁用 gzip 压缩大小报告
       reportCompressedSize: false,
+      // 自定义底层的 Rollup 打包配置
+      rollupOptions: {
+        output: {
+          // 指定 chunks 的入口文件模式
+          entryFileNames: 'static/js/[name]-[hash].js',
+          // 对代码分割中产生的 chunk 自定义命名
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          // 自定义构建结果中的静态资源名称
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+          // 压缩 Rollup 产生的额外代码
+          compact: true,
+          // 创建自定义的公共 chunk
+          manualChunks: {
+            vue: ['vue', 'vue-router', 'pinia'],
+          },
+        },
+      },
     },
   });
 };
